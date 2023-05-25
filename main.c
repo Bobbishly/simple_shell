d47300b5f13b9caf61d6f4da071bed9a87dac374
Ella <ellalearnscopy@gmail.com>
2023-05-25 04:09:32 +0100

    vcxzdsd

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+char *removeSpaces(char *str, int no);
+int strlength(char *str);
+
 /**
 * main - main function: where everything starts
 * Return: integer
@@ -49,3 +52,55 @@ int main()
 
     return (0);
 }
+
+
+/**
+* strlength - get a string's length
+* @str: the string
+* Return: length of string in integer
+*/
+int strlength(char *str)
+{
+  int len = 0;
+  
+  while (str[len])
+  {
+    len++;
+  }
+  
+  return (len);
+}
+
+/**
+* removeSpaces - function to remove spaces in a string
+* @str: the string
+* @no: length of string
+* Return: formatted string
+*/
+char *removeSpaces(char *str, int no)
+{
+  int isSpace = 0;
+  int isSpace2 = 0;
+  
+  int aCount = 0;
+  
+  while (aCount < no)
+  {
+    for (isSpace = 0; isSpace < no; isSpace++)
+    {
+      if (str[isSpace] == ' ')
+      {
+        isSpace2 = isSpace;
+        
+        while (str[isSpace2])
+        {
+          str[isSpace2] = str[isSpace2 + 1];
+          isSpace++;
+        }
+      }
+    }
+    aCount++;
+  }
+  return str;
+}
+
