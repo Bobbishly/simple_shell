3e88e537469382dfd07d5bf4e97ebcc95aeb0860
Ayomide Richard <104850785+Bobbishly@users.noreply.github.com>
2023-05-25 03:09:46 +0100

    function for string length

---
+++ strlength.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* strlength - get a string's length
+* @str: the string
+* Return: length of string in integer
+*/
+int strlength(char *str)
+{
+  int len = 0;
+  
+  while (str[len])
+  {
+    len++;
+  }
+  
+  return (len);
+}
