1596ca572fb84f7eedc2ec8626ca829733b52201
Ayomide Richard <104850785+Bobbishly@users.noreply.github.com>
2023-05-25 05:33:52 +0100

    kjnj

--- removeSpaces.c
+++ removeSpaces.c
@@ -8,27 +8,27 @@
 */
 char *removeSpaces(char *str, int no)
 {
-  int isSpace = 0;
-  int isSpace2 = 0;
-  
-  int aCount = 0;
-  
-  while (aCount < no)
-  {
-    for (isSpace = 0; isSpace < no; isSpace++)
-    {
-      if (str[isSpace] == ' ')
-      {
-        isSpace2 = isSpace;
-        
-        while (str[isSpace2])
-        {
-          str[isSpace2] = str[isSpace2 + 1];
-          isSpace++;
-        }
-      }
-    }
-    aCount++;
-  }
-  return str;
+int isSpace = 0;
+int isSpace2 = 0;
+
+int aCount = 0;
+
+while (aCount < no)
+{
+for (isSpace = 0; isSpace < no; isSpace++)
+{
+if (str[isSpace] == ' ')
+{
+isSpace2 = isSpace;
+
+while (str[isSpace2])
+{
+str[isSpace2] = str[isSpace2 + 1];
+isSpace++;
+}
+}
+}
+aCount++;
+}
+return str;
 }
